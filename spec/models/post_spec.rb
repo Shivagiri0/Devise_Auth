cb1522c5a6ea58ed1b11222714358933bd488b47
Shivagiri0 <Shivagiri617@gmail.com>
2023-09-20 10:17:30 +0545

    test-validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'is valid with valid attributes' do
-    user = User.create(email: 'user@user.com')
-    post = Post.new(title: 'Test Post', body: 'This is a test post.', user: user)
+  current_user = User.first_or_create!(email: 'user@user.com', password: 'user12', password_confirmation: 'user12')
+  it 'has a title' do
+    post = Post.new(title: '', body: 'This is body', user: current_user)
+    expect(post).to_not be_valid
+
+    post.title = 'has title'
     expect(post).to be_valid
   end
 
-  it 'is not valid without a title' do
-    post = Post.new(body: 'This is a test post.')
+  it 'has a body' do
+    post = Post.new(title: 'has a title', body: '', user: current_user)
     expect(post).to_not be_valid
+
+    post.body = 'a valid body'
+    expect(post).to be_valid
   end
 
-  it 'is not valid without body' do
-    post = Post.new(title: 'Test Post')
+  it 'has a title at least 2 characters long' do
+    post = Post.new(title:'', body: 'this is the body', user: current_user)
     expect(post).to_not be_valid
+
+    post.title = 'ab'
+    expect(post).to be_valid
   end
 
-  it 'belongs to a user' do
-    user = User.create(email: 'user@user.com')
-    post = Post.new(title: 'Test Post', body: 'This is a test post.', user: user)
-    expect(post.user).to eq(user)
+  it 'has a body between 10 to 100 characters' do
+    post = Post.new(title:'has a valid title', body: '', user: current_user)
+    expect(post).to_not be_valid
+
+    post.body = 'this is body'
+    expect(post).to be_valid
+
+    hundred_char_string = 'gK7qwxPruuSvyCnHCYbqfTkqpoGIwVSOF3PmEmxtfCzGonLB5jtuuUDM9lPIAh6qjJapcQDpaFiL3S3bSV4KBjRiW2GkspkM29Mv'
+    post.body = hundred_char_string
+    expect(post).to be_valid
   end
 end
