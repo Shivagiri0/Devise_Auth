76b95835cb75d00eaf204b4e44ab7ee5d26e8ec6
Shivagiri0 <Shivagiri617@gmail.com>
2023-09-21 07:55:28 +0545

    Request test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -45,4 +45,45 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to be_successful
     end
   end
+
+  describe 'GET /edit' do
+    it 'render successful response' do
+      post = Post.new(valid_attributes)
+      post.user = current_user
+      post.save
+      get edit_posts_url(post)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Post' do
+        expect do
+          post = Post.new(valid_attributes)
+          post.user = current_user
+          post.save
+          post posts_url, params: { post: valid_attributes }
+        end.to change(Post, :count).by(1)
+      end
+
+      it 'redirects to the created post' do
+        post posts_url, params: { post: valid_attributes }
+        expect(response).to be_successful
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Post' do
+        expect do
+          post posts_url, params: { post: invalid_attributes }
+        end.to change(Post, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post posts_url, params: { post: invalid_attributes }
+        expect(response).to be_successful
+      end
+    end
+  end
 end
